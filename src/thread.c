b2367d0c493334c950805a5311b40c597d3e5093
Alexander Pisarev <tresrus92@gmail.com>
2016-07-02 11:30:35 +0300

    Initial commit.

---
+++ src/thread.c
@@ -0,0 +1,8 @@
+#include "thread.h"
+#include <stdio.h>
+
+void test()
+{
+    printf("test");
+}
+
---
+++ src/thread.h
@@ -0,0 +1,7 @@
+#ifndef _SAGTM_THREAD_H_
+#define _SAGTM_THREAD_H_
+
+void test();
+
+#endif // _SAGTM_THREAD_H_
+
